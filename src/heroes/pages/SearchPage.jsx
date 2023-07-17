10006c390a5c6f35d2d186f8524c5241ca709e40
Carlos <charlyqv98@gmail.com>
2023-07-17 13:50:53 -0600

    Mostrar mensajes condicionales

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -14,6 +14,9 @@ export const SearchPage = () => {
 
   const heroes = getHeroesByName( q );
 
+  const showSearch = ( q.length === 0 );
+
+  const showError = ( q.length > 0 ) && heroes.length === 0;
 
   const { searchText, onInputChange } = useForm({
     searchText: q
@@ -21,7 +24,7 @@ export const SearchPage = () => {
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if ( searchText.trim().length <= 1 ) return;
+    // if ( searchText.trim().length <= 1 ) return;
 
     navigate(`?q=${ searchText }`);
   }
@@ -57,11 +60,18 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
+          {/* {
+            ( q === '' )
+              ? <div className="alert alert-primary">Search a hero</div>
+              : ( heroes.length === 0 )
+                && <div className="alert alert-danger">No hero with <b>{ q }</b></div>
+          } */}
+
+          <div className="alert alert-primary" style={{ display: showSearch ? '' : 'none' }}>
             Search a hero
           </div>
 
-          <div className="alert alert-danger">
+          <div className="alert alert-danger" style={{ display: showError ? '' : 'none' }}>
             No hero with <b>{ q }</b>
           </div>
 
